d5c51a66e2a1e21506f0cbfe0820fb564468473a
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-17 10:21:52 -0300

    test: should return sucess if all information provided

--- src/controllers/signup.spec.ts
+++ src/controllers/signup.spec.ts
@@ -30,4 +30,20 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error("Missing param: email"));
   });
+
+  it("Should return 200 if all information provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+      statusCode: Number,
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body.message).toEqual("Success to sign up.");
+  });
 });
