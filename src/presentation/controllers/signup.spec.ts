ab53cca24ac81a19e7c946d2838e86b443dbbe97
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-17 14:30:45 -0300

    test: ensure SignUpController calls EmailValidator with correct email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -96,6 +96,21 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
   });
 
+  it("should call EmailValidator with correct email", () => {
+    const { sut, emailValidatorStub } = makeSut();
+    const isValidSpy = jest.spyOn(emailValidatorStub, "isValid");
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    sut.handle(httpRequest);
+    expect(isValidSpy).toHaveBeenCalledWith("any_email@mail.com");
+  });
+
   it("should return 200 if all information is provided", () => {
     const httpRequest = {
       body: {
