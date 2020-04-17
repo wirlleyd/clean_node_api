280eb2ab559258d004332e50aa5bd8da8c3358e1
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-17 14:42:23 -0300

    feat: ensure SignUpController returns 500 if emailValidator throws

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,36 +4,43 @@ import { badRequest } from "../helpers/http-helper";
 import { Controller } from "../protocols/controller";
 import { EmailValidator } from "../protocols/email-validator";
 import { InvalidParamError } from "../erros/invalid-param-error";
+import { ServerError } from "../erros/server-error";
 export class SignUpController implements Controller {
   emailValidator: EmailValidator;
   constructor(emailValidator: EmailValidator) {
     this.emailValidator = emailValidator;
   }
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFileds = [
-      "name",
-      "email",
-      "password",
-      "passwordConfirmation",
-    ];
-    for (const field of requiredFileds) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const requiredFileds = [
+        "name",
+        "email",
+        "password",
+        "passwordConfirmation",
+      ];
+      for (const field of requiredFileds) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
+
+      const isValid = this.emailValidator.isValid(httpRequest.body.email);
+
+      if (!isValid) {
+        return badRequest(new InvalidParamError("email"));
+      }
+
+      return {
+        statusCode: 200,
+        body: {
+          message: "Success to sign up.",
+        },
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: new ServerError(),
+      };
     }
-
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-    console.log(isValid);
-
-    if (!isValid) {
-      return badRequest(new InvalidParamError("email"));
-    }
-
-    return {
-      statusCode: 200,
-      body: {
-        message: "Success to sign up.",
-      },
-    };
   }
 }
