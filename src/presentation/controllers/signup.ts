52376dcee273b02a8c0163a80f943d03c00f7b06
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-17 11:13:21 -0300

    feat: create signup controller

---
+++ src/presentation/controllers/signup.ts
@@ -0,0 +1,26 @@
+import { HttpRequest, HttpResponse } from "../protocols/http";
+
+export class SignUpController {
+  handle(httpRequest: HttpRequest): HttpResponse {
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: name"),
+      };
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: email"),
+      };
+    }
+
+    return {
+      statusCode: 200,
+      body: {
+        message: "Success to sign up.",
+      },
+    };
+  }
+}
