190684d51d36a24b0b4563a814cf75b6d2e085e2
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-17 23:51:12 -0300

    chore: add jest configs

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -44,6 +44,7 @@ export class SignUpController implements Controller {
       });
       return ok({ message: "Success to sign up.", user: account });
     } catch (error) {
+      console.error(error);
       return serverError();
     }
   }
