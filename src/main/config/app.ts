87cf83a442dea27db1964a9026201e0f7f536081
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-21 12:58:32 -0300

    feat: ensure signup routes returns an account on success

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import setupMiddelwares from "./middlewares";
-
+import setupRoutes from "./routes";
 const app = express();
 setupMiddelwares(app);
+setupRoutes(app);
 
 export default app;
