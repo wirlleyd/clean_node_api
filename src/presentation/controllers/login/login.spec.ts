48058f5fb53c4467d4a72e1597d7656c5cacfdf7
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-23 16:58:02 -0300

    test: ensure LoginController returns 400 if no passwords provided

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -12,4 +12,15 @@ describe("Login Controller", () => {
     const httpResponse = await sut.handle(httpRequest);
     expect(httpResponse).toEqual(badRequest(new MissingParamError("email")));
   });
+
+  it("Should return 400 if no password provider", async () => {
+    const sut = new LoginController();
+    const httpRequest = {
+      body: {
+        email: "any_email",
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse).toEqual(badRequest(new MissingParamError("password")));
+  });
 });
