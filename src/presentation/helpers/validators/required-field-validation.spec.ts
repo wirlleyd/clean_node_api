657743a0c9a330eb687a9b305465e6bb05ea682c
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-27 19:42:02 -0300

    test: ensuer RequiredFieldValidation dont return any value if validation succeeds

--- src/presentation/helpers/validators/required-field-validation.spec.ts
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -7,4 +7,9 @@ describe("RequiredField Validation", () => {
     const error = sut.validate({ name: "any_name" });
     expect(error).toEqual(new MissingParamError("field"));
   });
+  it("Shoul not return error if validation succeeds", () => {
+    const sut = new RequiredFieldValidation("field");
+    const error = sut.validate({ field: "any_field" });
+    expect(error).toBeFalsy();
+  });
 });
