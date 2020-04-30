e15e2a894b1f965890ec28e57eed6810c808eb8e
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-30 11:31:35 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmail success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,11 +2,19 @@ import { AddAccountRepository } from "../../../../data/protocols/db/add-account-
 import { AddAccountModel } from "../../../../domain/usecases/add-account";
 import { AccountModel } from "../../../../domain/models/account";
 import { MongoHelper } from "../helper/mongo-helper";
-export class AccountMongoRepository implements AddAccountRepository {
+import { LoadAccountByEmailRepository } from "../../../../data/protocols/db/load-accountBy-email-repository";
+export class AccountMongoRepository
+  implements AddAccountRepository, LoadAccountByEmailRepository {
   async add(account: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection("accounts");
     const result = await accountCollection.insertOne(account);
     const accountData = MongoHelper.map(result.ops[0]);
     return accountData;
+  }
+
+  async loadByEmail(email: String): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection("accounts");
+    const account = await accountCollection.findOne({ email });
+    return account && MongoHelper.map(account);
   }
 }
