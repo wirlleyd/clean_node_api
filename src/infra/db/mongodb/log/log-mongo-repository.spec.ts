4bcbb377bbea5d9ea18085e981cdf31e7a66de5e
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-30 14:07:41 -0300

    refactor: rename log and account mongo repository

--- src/infra/db/mongodb/log/log-mongo-repository.spec.ts
+++ src/infra/db/mongodb/log/log-mongo-repository.spec.ts
@@ -1,6 +1,6 @@
 import { MongoHelper } from "../helper/mongo-helper";
 import { Collection } from "mongodb";
-import { LogMongoRepository } from "./log";
+import { LogMongoRepository } from "./log-mongo-repository";
 describe("Log Mongo Repository", () => {
   let errorCollection: Collection;
   beforeAll(async () => {
