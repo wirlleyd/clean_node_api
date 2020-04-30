fcefd0c4cd8b7bc389cab88f701b9806904d70b1
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-30 10:45:35 -0300

    test: ensure JwtAdapter throws if sign throw

--- src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter/jwt-adapter.spec.ts
@@ -20,4 +20,13 @@ describe("Jwt Adapter", () => {
     const token = await sut.encrypt("any_id");
     expect(token).toBe("any_token");
   });
+
+  test("Should JwtAdapter throws if sign throws", async () => {
+    const sut = new JwtAdapter("secret");
+    jest.spyOn(jwt, "sign").mockImplementationOnce(() => {
+      throw new Error();
+    });
+    const token = sut.encrypt("any_id");
+    await expect(token).rejects.toThrow();
+  });
 });
