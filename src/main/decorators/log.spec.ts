14fa525f18948f86d223368dad009d9cea102709
Wirlley Delfino <wirlley_d@hotmail.com>
2020-04-30 14:29:01 -0300

    refactor: move LogErrorRepository to a folder

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -2,7 +2,7 @@ import { LogControllerDecorator } from "./log";
 import { Controller } from "../../presentation/protocols";
 import { HttpRequest, HttpResponse } from "../../presentation/protocols";
 import { serverError } from "../../presentation/helpers/http/http-helper";
-import { LogErrorRepository } from "../../data/protocols/db/log-error-repository";
+import { LogErrorRepository } from "../../data/protocols/db/log/log-error-repository";
 interface SutTypes {
   sut: LogControllerDecorator;
   controllerStub: Controller;
