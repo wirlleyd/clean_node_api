eecef6b8a4f13cb242ba438513f85bbff46e08b0
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-02 10:46:14 -0300

    test: ensure Login routes return 200 on success and 401 with login not find any account

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -1,6 +1,10 @@
 import request from "supertest";
 import app from "../config/app";
 import { MongoHelper } from "../../infra/db/mongodb/helper/mongo-helper";
+import { Collection } from "mongodb";
+import { hash } from "bcrypt";
+
+let accountCollection: Collection;
 
 describe("Login Routes", () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe("Login Routes", () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection("accounts");
+    accountCollection = await MongoHelper.getCollection("accounts");
     await accountCollection.deleteMany({});
   });
 
@@ -27,6 +31,44 @@ describe("Login Routes", () => {
           passwordConfirmation: "123456",
         })
         .expect(200);
+    });
+  });
+
+  describe("POST /login", () => {
+    it("Should return 200 on login", async () => {
+      const password = await hash("123", 12);
+
+      await accountCollection.insertOne({
+        name: "Test",
+        email: "test@mail.com",
+        password,
+      });
+
+      await request(app)
+        .post("/api/login")
+        .send({
+          email: "test@mail.com",
+          password: "123",
+        })
+        .expect(200);
+    });
+
+    it("Should return 401 on login credentials not match", async () => {
+      const password = await hash("123", 12);
+
+      await accountCollection.insertOne({
+        name: "Test",
+        email: "test@mail.com",
+        password,
+      });
+
+      await request(app)
+        .post("/api/login")
+        .send({
+          email: "test@mail.com",
+          password: "1234",
+        })
+        .expect(401);
     });
   });
 });
--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { makeSignUpController } from "../factories/signup/sign-up";
+import { makeLoginControler } from "../factories/login/login-factory";
 import { adaptRoute } from "../adapters/express-routes-adapter";
 
 export default (router: Router): void => {
   router.post("/signup", adaptRoute(makeSignUpController()));
+  router.post("/login", adaptRoute(makeLoginControler()));
 };
