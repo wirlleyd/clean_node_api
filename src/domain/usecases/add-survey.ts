123b89f7cfd9c6c1164f28e56b6c895080896ccc
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-12 20:59:00 -0300

    feat: ensure AddSurveyController calls addSurvey usecase with correct values

---
+++ src/domain/usecases/add-survey.ts
@@ -0,0 +1,15 @@
+import { AccountModel } from "../models/account";
+
+type Answer = {
+  image: string;
+  answer: string;
+};
+
+export interface AddSurveyModel {
+  question: string;
+  answers: Answer[];
+}
+
+export interface AddSurvey {
+  add(data: AddSurveyModel): Promise<void>;
+}
--- src/presentation/controllers/survey/add-survey/add-survey-controler.ts
+++ src/presentation/controllers/survey/add-survey/add-survey-controler.ts
@@ -1,17 +1,22 @@
 import { Controller, HttpRequest, HttpResponse } from "../../../protocols";
 import { Validation } from "../../../helpers/validators/validation";
 import { badRequest } from "../../../helpers/http/http-helper";
+import { AddSurvey } from "../../../../domain/usecases/add-survey";
 
 export class AddSurveyController implements Controller {
   validation: Validation;
-  constructor(validation: Validation) {
+  addSurvey: AddSurvey;
+  constructor(validation: Validation, addSurvey: AddSurvey) {
     this.validation = validation;
+    this.addSurvey = addSurvey;
   }
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const error = this.validation.validate(httpRequest.body);
     if (error) {
       return badRequest(error);
     }
+    const { question, answers } = httpRequest.body;
+    await this.addSurvey.add({ question, answers });
     return new Promise((res) => res(null));
   }
 }
