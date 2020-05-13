eb8ba39cbd19e1a7d2bc7587a8060c64345d8c33
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-13 15:41:35 -0300

    feat: ensure AuthMiddleware return a accountId if load return an account

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -15,7 +15,7 @@ export class AuthMiddleware implements Middleware {
     if (accessToken) {
       const account = await this.loadAccountByToken.load(accessToken);
       if (account) {
-        return ok(account);
+        return ok({ accountId: account.id });
       }
     }
     return forbidden(new AccessDeniedError());
