72ce2c40991927c3bc48a092c736b880bca59d3e
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-13 14:31:23 -0300

    test: ensure AuthMiddleware calls LoadAccountByToken with correct token

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -2,14 +2,34 @@ import { HttpRequest } from "../protocols";
 import { forbidden } from "../helpers/http/http-helper";
 import { AccessDeniedError } from "../erros/access-denied-error";
 import { AuthMiddleware } from "./auth-middleware";
+import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token";
+import { AccountModel } from "../../domain/models/account";
 
+const makeFakeAccount = (): AccountModel => ({
+  id: "any_id",
+  name: "any_name",
+  email: "any_email@mail.com",
+  password: "hashed_password",
+});
+
+const makeLoadAccountStub = (): LoadAccountByToken => {
+  class LoadAccountByTokenStub implements LoadAccountByToken {
+    load(accessToken: String, role?: string): Promise<AccountModel> {
+      return new Promise((res) => res(makeFakeAccount()));
+    }
+  }
+  return new LoadAccountByTokenStub();
+};
 interface SutTypes {
   sut: AuthMiddleware;
+  loadAccountByTokenStub: LoadAccountByToken;
 }
 const makeSut = (): SutTypes => {
-  const sut = new AuthMiddleware();
+  const loadAccountByTokenStub = makeLoadAccountStub();
+  const sut = new AuthMiddleware(loadAccountByTokenStub);
   return {
     sut,
+    loadAccountByTokenStub,
   };
 };
 
@@ -18,5 +38,16 @@ describe("Auth Middleware", () => {
     const { sut } = makeSut();
     const httpResponse = await sut.handle({});
     expect(httpResponse).toEqual(forbidden(new AccessDeniedError()));
+  });
+
+  it("Should call LoadAccountByToken with correct access token", async () => {
+    const { sut, loadAccountByTokenStub } = makeSut();
+    const loadSpy = jest.spyOn(loadAccountByTokenStub, "load");
+    await sut.handle({
+      headers: {
+        "x-access-token": "any_token",
+      },
+    });
+    expect(loadSpy).toHaveBeenCalledWith("any_token");
   });
 });
