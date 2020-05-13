d3906961c92f70d9722805b2769818e59c131f84
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-13 19:06:23 -0300

    test: ensure DbLoadAccountByToken return null if LoadAccountByTokenRepository return null

--- src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
+++ src/data/usecases/load-account-by-token/db-load-account-by-token.spec.ts
@@ -73,4 +73,13 @@ describe("DbLoadAccountByToken UseCase", () => {
     await sut.load("any_token", "any_role");
     expect(loadByTokenSpy).toHaveBeenCalledWith("any_token", "any_role");
   });
+
+  it("Should return null if LoadAccountByTokenRepository return null", async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut();
+    jest
+      .spyOn(loadAccountByTokenRepositoryStub, "loadByToken")
+      .mockReturnValueOnce(new Promise((res) => res(null)));
+    const account = await sut.load("any_token", "any_role");
+    expect(account).toBeNull();
+  });
 });
