0510ec238f45c0dc771a8ab8e8793cc7040f9fd4
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-13 15:28:35 -0300

    refactor: move request creation to a factory method

--- src/presentation/middlewares/auth-middleware.spec.ts
+++ src/presentation/middlewares/auth-middleware.spec.ts
@@ -1,9 +1,15 @@
-import { HttpRequest } from "../protocols";
 import { forbidden } from "../helpers/http/http-helper";
 import { AccessDeniedError } from "../erros/access-denied-error";
 import { AuthMiddleware } from "./auth-middleware";
 import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token";
 import { AccountModel } from "../../domain/models/account";
+import { HttpRequest } from "../protocols";
+
+const makeFakeHttpRequest = (): HttpRequest => ({
+  headers: {
+    "x-access-token": "any_token",
+  },
+});
 
 const makeFakeAccount = (): AccountModel => ({
   id: "any_id",
@@ -24,6 +30,7 @@ interface SutTypes {
   sut: AuthMiddleware;
   loadAccountByTokenStub: LoadAccountByToken;
 }
+
 const makeSut = (): SutTypes => {
   const loadAccountByTokenStub = makeLoadAccountStub();
   const sut = new AuthMiddleware(loadAccountByTokenStub);
@@ -43,11 +50,7 @@ describe("Auth Middleware", () => {
   it("Should call LoadAccountByToken with correct access token", async () => {
     const { sut, loadAccountByTokenStub } = makeSut();
     const loadSpy = jest.spyOn(loadAccountByTokenStub, "load");
-    await sut.handle({
-      headers: {
-        "x-access-token": "any_token",
-      },
-    });
+    await sut.handle(makeFakeHttpRequest());
     expect(loadSpy).toHaveBeenCalledWith("any_token");
   });
 });
