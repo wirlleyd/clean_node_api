74fd66e20afa4bc22d5805f0302fdbe0bd5d540f
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-13 15:36:24 -0300

    feat: ensure AuthMiddleware return 403 if return null and 200 if find a account

--- src/presentation/middlewares/auth-middleware.ts
+++ src/presentation/middlewares/auth-middleware.ts
@@ -1,6 +1,6 @@
 import { Middleware } from "../protocols/middleware";
 import { HttpRequest, HttpResponse } from "../protocols";
-import { forbidden } from "../helpers/http/http-helper";
+import { forbidden, ok } from "../helpers/http/http-helper";
 import { AccessDeniedError } from "../erros/access-denied-error";
 import { LoadAccountByToken } from "../../domain/usecases/load-account-by-token";
 
@@ -13,7 +13,10 @@ export class AuthMiddleware implements Middleware {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const accessToken = httpRequest.headers?.["x-access-token"];
     if (accessToken) {
-      await this.loadAccountByToken.load(accessToken);
+      const account = await this.loadAccountByToken.load(accessToken);
+      if (account) {
+        return ok(account);
+      }
     }
     return forbidden(new AccessDeniedError());
   }
