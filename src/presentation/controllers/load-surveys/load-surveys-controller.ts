bc90b0e94bfc53a8bb1e63b1dfd47fc8fe357d82
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-19 20:53:46 -0300

    feat: ensure  LoadSurveysController return 204 if load survey return an empty array

--- src/presentation/controllers/load-surveys/load-surveys-controller.ts
+++ src/presentation/controllers/load-surveys/load-surveys-controller.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpRequest, HttpResponse } from "../../protocols";
 import { LoadSurveys } from "../../../domain/usecases/load-surveys";
-import { ok, serverError } from "../../helpers/http/http-helper";
+import { ok, serverError, noContent } from "../../helpers/http/http-helper";
 
 export class LoadSurveysController implements Controller {
   loadSurveys: LoadSurveys;
@@ -11,7 +11,7 @@ export class LoadSurveysController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load();
-      return ok(surveys);
+      return surveys.length ? ok(surveys) : noContent();
     } catch (error) {
       return serverError(error);
     }
