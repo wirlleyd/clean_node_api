5ce19eafa646381d73fe694a3dab103e1c255ad6
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-20 21:04:44 -0300

    feat: ensure SurveyMongoRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,9 +1,17 @@
 import { AddSurveyRepository } from "../../../../data/protocols/db/survey/add-survey-repository";
 import { AddSurveyModel } from "../../../../domain/usecases/add-survey";
 import { MongoHelper } from "../helper/mongo-helper";
-export class SurveyMongoRepository implements AddSurveyRepository {
+import { LoadSurveyRepository } from "../../../../data/protocols/db/survey/load-survey-repository";
+import { SurveyModel } from "../../../../domain/models/survey";
+export class SurveyMongoRepository
+  implements AddSurveyRepository, LoadSurveyRepository {
   async add(surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection("surveys");
     await surveyCollection.insertOne(surveyData);
+  }
+  async loadAll(): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoHelper.getCollection("surveys");
+    const surveys: SurveyModel[] = await surveyCollection.find().toArray();
+    return surveys;
   }
 }
