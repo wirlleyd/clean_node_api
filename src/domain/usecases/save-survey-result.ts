ef66c0aa94044930c793154527adf2b5574af9b9
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-22 18:44:33 -0300

    feat: add LoadSurveyById usecase

---
+++ src/domain/usecases/save-survey-result.ts
@@ -0,0 +1,9 @@
+import { AccountModel } from "../models/account";
+import { Answer } from "../models/survey";
+import { SurveyResultModel } from "../models/survey-result";
+
+export type SaveSurveyResultModel = Omit<SurveyResultModel, "id">;
+
+export interface SaveSurveyResult {
+  save(data: SaveSurveyResultModel): Promise<SurveyResultModel>;
+}
