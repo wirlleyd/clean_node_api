567b0f1f9e5fe4681d49e0971ce9fbcccb0671e6
Wirlley Delfino <wirlley_d@hotmail.com>
2020-05-25 13:49:10 -0300

    feat: add method to map an array of properties on MongoHelper

--- src/infra/db/mongodb/helper/mongo-helper.ts
+++ src/infra/db/mongodb/helper/mongo-helper.ts
@@ -23,8 +23,12 @@ export const MongoHelper = {
     return this.client.db().collection(name);
   },
 
-  map: (collection: any): any => {
-    const { _id, ...collectionWithoutId } = collection;
+  map: (data: any): any => {
+    const { _id, ...collectionWithoutId } = data;
     return { id: _id, ...collectionWithoutId };
+  },
+
+  mapArray: (collection: any[]): any[] => {
+    return collection.map((c) => MongoHelper.map(c));
   },
 };
